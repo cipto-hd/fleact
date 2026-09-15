0598a06603ff06504c7b4fd5c2d29c2ca1ebb38b
Cipto Hadi <ciptohadi79@gmail.com>
2026-09-15 17:07:40 +0700

    fix: remove /app redirect from root

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-header('Location: app');
+/** Absolute path to project root */
+const ROOT = __DIR__;
+
+require ROOT . '/vendor/autoload.php';
+
+Flight::set('flight.views.path', ROOT . '/src/views');
+require ROOT . '/src/routes/index.php';
+Flight::start();
